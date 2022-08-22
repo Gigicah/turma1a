3ab4a1696d89323625699078b6869389b3e9e705
Gigicah <111289169+Gigicah@users.noreply.github.com>
2022-08-22 09:49:08 -0300

    Delete gio.py

--- gio.py
+++
@@ -1,7 +0,0 @@
-#-*-coding:utf8;-*-
-#qpy:console
-
-print("calculadora da segunda lei de Newton")
-massa = int(input("qual é a aceleração do corpo?"))
-aceleracao = int(input("qual é a aceleração do corpo?"))
-print("A força em Newton é equivalente a" , massa * aceleracao, "Newtons" )
